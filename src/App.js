9a4c53369de632175d19bbf0bb899f3f2796d1a9
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-20 18:45:30 +0100

    Passing references between components

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        { name: 'Maximus', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 31 },
         { name: 'Sarah', age: 27 },
       ]
@@ -27,14 +27,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maxumus!!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age} 
-          click={this.switchNameHandler} >My hobbies ...</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')} >My hobbies ...</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
