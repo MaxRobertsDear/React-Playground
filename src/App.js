f7567c242278863657696d4c2d1b238e133df0d8
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-20 15:48:58 +0100

    Using state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <Person />
-      <Person />
-      <Person />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 31 },
+      { name: 'Sarah', age: 26 },
+      
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }  
 }
 
 export default App;
